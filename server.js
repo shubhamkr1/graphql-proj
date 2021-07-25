b47c1890fff701cb6b2c32bcb83707569b96d261
Shubham Kumar <shubhamkumar007@gmail.com>
2021-07-25 17:20:44 -0400

    adding query for single author,book

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ const AuthorType = new GraphQLObjectType({
     fields: () => ({
         id: { type : GraphQLNonNull(GraphQLInt)},
         name: { type: GraphQLNonNull(GraphQLString)},
+        books: {
+            type: new GraphQLList(BookType),
+            resolve: (author) => {
+              return books.filter(book => book.authorId === author.id)
+            }
+        }
      
     })
 })
@@ -62,6 +68,14 @@ const RootQueryType = new GraphQLObjectType({
     name:'Query',
     description: 'Root Query',
     fields: () => ({
+        book: {
+            type: BookType,
+            description: 'A single book',
+            args:{
+               id: { type: GraphQLInt}
+            },
+            resolve: (parent,args) => books.find(book => book.id === args.id)
+        },
         books: {
             type: new GraphQLList(BookType),
             description: 'List of all books',
@@ -71,6 +85,14 @@ const RootQueryType = new GraphQLObjectType({
             type: new GraphQLList(AuthorType),
             description: 'List of all Authors',
             resolve: () => authors
+        },
+        author: {
+            type: AuthorType,
+            description: 'A Single Author',
+            args: {
+              id: { type: GraphQLInt }
+            },
+            resolve: (parent, args) => authors.find(author => author.id === args.id)
         }
     })
 })
